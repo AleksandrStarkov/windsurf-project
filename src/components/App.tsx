6de70554b97fab5d867775967da226ab21a22883
Aleksandr <102595282+AleksandrStarkov@users.noreply.github.com>
2023-02-22 17:20:12 +0200

    adf

---
+++ src/components/App.tsx
@@ -0,0 +1,29 @@
+// import { AuthPage } from './Form/auth-page';
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//       }}
+//     >
+//       <AuthPage />
+//     </div>
+//   );
+// };
+
+import { AuthPage } from './Form/auth-page';
+
+function App() {
+  return (
+    <div className="app">
+      <AuthPage />
+    </div>
+  );
+}
+
+export default App;
